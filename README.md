4f54b945fc960d4514b4567ecbc87ba51ea59eb0
rsancho64 <rsancho@iesrioarba.es>
2023-10-30 12:39:58 +0100

    .

---
+++ README.md
@@ -0,0 +1,11 @@
+# listas.studio
+
+Estudio de listas como estructuras dinamicas
+
+Empezamos con python (es mas facil...)
+
+```python
+
+# lista now: [22, 55, 11]
+```
+
---
+++ lista.py
@@ -0,0 +1,15 @@
+lista = []
+lista.append(22)
+lista.append(55)
+lista.append(11)
+print(lista)
+
+lista2 = lista.copy()
+print(lista2)
+
+lista2.append("END")
+print(lista2)
+
+lista2.clear()
+print(lista2)
+
