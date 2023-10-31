2379f826c6418aaa16fed9bbde8bd3acfeb018c0
rsancho64 <rsancho@iesrioarba.es>
2023-10-31 13:33:30 +0100

    .

--- README.md
+++ README.md
@@ -2,10 +2,40 @@
 
 Estudio de listas como estructuras dinamicas
 
-Empezamos con python (es mas facil...)
+## Empezamos con python (es mas facil...)
 
 ```python
+lista = []
+lista.append(22)
+lista.append(55)
+lista.append(11)
+print(lista)
 
-# lista now: [22, 55, 11]
+lista2 = lista.copy()
+print(lista2)
+
+lista2.append("END")
+print(lista2)
+
+lista2.clear()
+print(lista2)
+
+# INTROSPECCION EN PYTHON
+# https://stackoverflow.com/questions/1006169/how-do-i-look-inside-a-python-object
+
+print(dir(lista2))
+## try: lista.__dir__
+## try: dir(lista)
+## try: type(lista)
+## try: isistance(lista,list)
 ```
+
+## listas enlazadas (¿tipo C?) [¿OO?]
+
+Códigos de: [**aqui**](https://geekflare.com/es/python-linked-lists/) 
+
+En una lista enlazada simple cada nodo contiene un -único- puntero conectado al siguiente -nodo- -de la lista enlazada-. 
+
+Para cada nodo de la lista enlazada tenemos que gestionar el dato que contiene y el enlace al -eventual- siguiente nodo.
+
 
--- lista.py
+++ lista.py
@@ -1,15 +1,46 @@
-lista = []
-lista.append(22)
-lista.append(55)
-lista.append(11)
-print(lista)
+class Nodo:
 
-lista2 = lista.copy()
-print(lista2)
+    def __init__(self, datos):
+        self.datos = datos
+        self.siguiente = None
 
-lista2.append("END")
-print(lista2)
+    def printNodo(self):
+        print(f"<{self.datos};{self.siguiente}>")
 
-lista2.clear()
-print(lista2)
+    def __str__(self):
+        # return f"<{self.datos};{self.siguiente}>"
+        return f"<{self.datos}>"    
+
+class ListaEnlazadaSimple:
+    def __init__(self):
+        self.head = None
+
+if __name__ == '__main__':
+
+    n1 = Nodo(11)
+    n2 = Nodo("ramon")
+
+    # impresion...
+    # 1. sin un servicio del objeto
+    # print(n1.datos)
+    # print(n1.siguiente)
+    # print(n1) # sale repr()
+    # print(n2) # sale repr()
+
+    # print(n2.datos)
+    # print(n2.siguiente)
+
+    # 2. con un servicio del objeto
+    # n1.printNodo()
+    # n2.printNodo()
+
+    # 3. con la expresion del objeto como string: str() ... repr()
+    # https://www.digitalocean.com/community/tutorials/python-str-repr-functions  
+
+    print(n1) # ok, existe __str__()
+    print(n2) # ok, existe __str__()   
+
+    # Cosa similar: serialización -marshalling- (similar!!)
+    # https://es.wikipedia.org/wiki/Serializaci%C3%B3n)
+
 
